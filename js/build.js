493e11fdac959318bcb6fb8e5dd8492023f32341
Joao-pina-fernandes <Joao.Pina.Fernandes@gmail.com>
2016-11-09 12:05:36 +0000

    widget first iteration

---
+++ js/build.js
@@ -0,0 +1,143 @@
+$('[data-login-id]').each(function(){
+  var _this = this;
+  _this.$container = $(this);
+  _this.id = _this.$container.attr('data-login-id');
+  _this.data = Fliplet.Widget.getData(_this.id);
+
+  _this.pvName = 'login_component_' + _this.id;
+  var dataStructure = {
+    auth_token: '',
+    id: '',
+    email: '',
+    createdAt: null
+  };
+
+  _this.$container.on('submit', (function (event) {
+    event.preventDefault();
+
+    var userEmail = _this.$container.find('.email_input').val();
+    var userPassword = _this.$container.find('.pass_input').val();
+    if(Fliplet.Env.get('platform') === 'web'){
+      login({
+        'email': userEmail,
+        'password' : userPassword
+      }).then(function(response) {
+        _this.loginPV = response;
+        Fliplet.Security.Storage.update().then(function(){
+          return validateAppAccess();
+        });
+      }).then(function(){
+        Fliplet.Navigate.to(_this.data.action);
+      },function(){
+        alert("Not a valid Fliplet account login");
+      });
+    } else {
+      Fliplet.Native.Authentication.loginUser({
+        email: userEmail,
+        password: userPassword
+      }).then(function() {
+        return validateAppAccess();
+      }).then(function(){
+        Fliplet.Navigate.to(_this.data.action);
+      },function(){
+        navigator.alert("Not a valid Fliplet account login");
+      });
+    }
+
+  }));
+
+  function init(){
+    Fliplet.Security.Storage.init().then(function(){
+      Fliplet.Security.Storage.create(_this.pvName, dataStructure).then(
+        function(data) {
+          _this.loginPV = data;
+
+          if(Fliplet.Env.get('platform') === 'web') {
+            validateWeb().then(function(){
+              return validateAppAccess();
+            }).then(function(){
+              Fliplet.Navigate.to(_this.data.action);
+            },function(){
+              _this.$container.removeClass('hidden');
+            });
+          } else {
+            Fliplet.User.setUserDetails(_this.loginPV);
+            Fliplet.Native.Authentication.saveUserDetails(_this.loginPV).then(function(){
+              return Fliplet.Native.Authentication.verifyLogin();
+            }).then(function(){
+              return validateAppAccess();
+            }).then(function(){
+              Fliplet.Navigate.to(_this.data.action);
+            },function(){
+              _this.$container.removeClass('hidden');
+            })
+          }
+        }
+      );
+    });
+  }
+
+  function validateAppAccess(){
+    return new Promise(function(resolve, reject){
+      getApps().then(function(apps) {
+        if(_.find(apps,{id: Fliplet.Env.get('appId')})) {
+          return resolve();
+        }
+        return reject();
+      });
+    });
+  }
+
+
+  function validateWeb(){
+    //validate token
+    return request({
+      'method' : 'GET',
+      'url' : 'v1/user',
+      'auth_token' : _this.loginPV.auth_token
+    });
+  }
+
+  function login(options) {
+    return request({
+      'method' : 'POST',
+      'url' : 'v1/auth/login',
+      'data' : {
+        'email' : options.email,
+        'password' : options.password
+      }
+    });
+  }
+
+  function request(data){
+    //validate token
+    return Fliplet.Navigator.onReady().then(function () {
+      data.url = Fliplet.Env.get('apiUrl') + data.url;
+      data.headers = data.headers || {};
+      data.headers['Auth-token'] = data.token;
+      return $.ajax(data);
+    });
+  }
+
+  function getApps(){
+    var apps = [];
+
+    if(Fliplet.Env.get('platform') === 'web'){
+      return request({
+        url: 'v1/apps',
+        data: {
+          'auth_token' : _this.loginPV.auth_token
+        }
+      });
+    } else {
+      return Fliplet.Apps.get();
+    }
+  }
+
+  if(Fliplet.Env.get('platform') === 'web') {
+     init();
+    _this.$container.parent().on("fliplet_page_reloaded", init);
+  } else {
+    document.addEventListener("deviceready", init);
+  }
+});
---
+++ js/interface.js
@@ -0,0 +1,51 @@
+var widgetId = Fliplet.Widget.getDefaultId();
+var data = Fliplet.Widget.getData(widgetId) || {};
+var fields = [''];
+
+var linkActionProvider = Fliplet.Widget.open('com.fliplet.link', {
+  // If provided, the iframe will be appended here,
+  // otherwise will be displayed as a full-size iframe overlay
+  selector: '#action',
+  // Also send the data I have locally, so that
+  // the interface gets repopulated with the same stuff
+  data: data.action,
+  // Events fired from the provider
+  onEvent: function (event, data) {
+    if (event === 'interface-validate') {
+      Fliplet.Widget.toggleSaveButton(data.isValid === true);
+    }
+  }
+});
+
+// 1. Fired from Fliplet Studio when the external save button is clicked
+Fliplet.Widget.onSaveRequest(function () {
+  $('form').submit();
+});
+
+// 2. Fired when the user submits the form
+$('form').submit(function (event) {
+  event.preventDefault();
+  linkActionProvider.forwardSaveRequest();
+});
+
+// 3. Fired when the provider has finished
+linkActionProvider.then(function (result) {
+  data.action = result.data;
+  save(true);
+});
+
+function save(notifyComplete) {
+  
+  Fliplet.Widget.save(data).then(function () {
+    if (notifyComplete) {
+      Fliplet.Widget.complete();
+      window.location.reload();
+    } else {
+      Fliplet.Studio.emit('reload-widget-instance', widgetId);
+    }
+  });
+}
+
+$('#help_tip').on('click', function() {
+  alert("During beta, please use live chat and let us know what you need help with.");
+});
