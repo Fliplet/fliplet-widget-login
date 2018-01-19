4bdc61c4d636c826cf4f94de85c48e61445aff1b
Ricardo Seromenho <ricardo@seromenho.com>
2018-01-19 17:19:18 +0000

    Use sessions

--- js/build.js
+++ js/build.js
@@ -65,14 +65,10 @@ $('[data-login-id]').each(function() {
       _this.loginPV.auth_token = response.auth_token;
       _this.loginPV.email = response.email;
 
-      return Fliplet.Security.Storage.update().then(function() {
-        return Fliplet.App.Storage.set(_this.pvNameStorage, {
-          userRoleId: response.userRoleId,
-          auth_token: response.auth_token,
-          email: response.email
-        }).then(function() {
-          return validateAppAccess();
-        });
+      return Fliplet.App.Storage.set(_this.pvNameStorage, {
+        userRoleId: response.userRoleId,
+        auth_token: response.auth_token,
+        email: response.email
       });
     }).then(function() {
       _this.$container.find('.btn-login').removeClass('disabled');
@@ -146,14 +142,11 @@ $('[data-login-id]').each(function() {
       _this.loginPV.userRoleId = userData.userRoleId;
       _this.loginPV.auth_token = userData.auth_token;
       _this.loginPV.email = userData.email;
-      return Fliplet.Security.Storage.update().then(function() {
-        return Fliplet.App.Storage.set(_this.pvNameStorage, {
-          auth_token: userData.auth_token,
-          userRoleId: userData.userRoleId,
-          email: userData.email
-        }).then(function() {
-          return validateAppAccess();
-        });
+      
+      return Fliplet.App.Storage.set(_this.pvNameStorage, {
+        auth_token: userData.auth_token,
+        userRoleId: userData.userRoleId,
+        email: userData.email
       });
     }).then(function() {
       _this.$container.find('.two-factor-btn').removeClass('disabled');
@@ -185,8 +178,8 @@ $('[data-login-id]').each(function() {
     _this.loginPV = {};
     Fliplet.User.getCachedSession()
       .then(function(session) {
-        if (session && session.accounts && session.accounts.flipletLogin) {
-          _this.loginPV = session.accounts.flipletLogin[0];
+        if (session && session.server && session.server.passports && session.server.passports.flipletLogin) {
+          _this.loginPV = session.server.passports.flipletLogin[0];
 
           if (!Fliplet.Navigator.isOnline() && !Fliplet.Env.get("disableSecurity")) {
             Fliplet.Navigate.to(_this.data.action);
