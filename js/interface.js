ee0d5ac08ef6913b077d6cc09d7b43f2cb59cebb
Maksym Shokin <maksym.sh@upplabs.com>
2020-06-25 13:58:52 +0300

    security-warning

--- js/interface.js
+++ js/interface.js
@@ -1,5 +1,6 @@
 var widgetId = Fliplet.Widget.getDefaultId();
 var data = Fliplet.Widget.getData(widgetId) || {};
+var appId = Fliplet.Env.get('appId');
 var headingValue = data.heading || 'Welcome to the login of this app';
 $('#login_heading').val(headingValue).trigger('change');
 
@@ -8,6 +9,8 @@ var omitPages = page ? [page.id] : [];
 
 data.action = data.action || {};
 data.action.omitPages = omitPages;
+
+checkSecurityRules();
 
 var linkActionProvider = Fliplet.Widget.open('com.fliplet.link', {
   // If provided, the iframe will be appended here,
@@ -53,6 +56,39 @@ function save(notifyComplete) {
   });
 }
 
+// Shows warning if security setting are not configured correctly
+function checkSecurityRules () {
+  Fliplet.API.request('v1/apps/' + appId).then(function(result) {
+    if (!result || !result.app) {
+      return;
+    }
+
+    var hooks = _.get(result.app, 'hooks', []);
+    var isSecurityConfigured = _.some(hooks, function(hook) {
+      return hook.script.indexOf(page.id) !== -1;
+    });
+
+    if (!hooks.length) {
+      $('#security-alert span').text('app has no security rules configured to prevent unauthorized access.');
+    }
+
+    $('#security-alert').toggleClass('hidden', isSecurityConfigured);
+  })
+}
+
 $('#login_heading').on('keyup change paste blur', $.debounce(function() {
   save();
 }, 500));
+
+// Open security overlay
+$('#security-alert u').on('click', function() {
+  Fliplet.Studio.emit('overlay', {
+    name: 'app-settings',
+    options: {
+      title: 'App Settings',
+      size: 'large',
+      section: 'appSecurity',
+      appId: appId
+    }
+  });
+});
